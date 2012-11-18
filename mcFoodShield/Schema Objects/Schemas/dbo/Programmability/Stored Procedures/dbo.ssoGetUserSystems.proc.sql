01e997311222b281ede0a25934b3242975154906
ccarson <chris.carson.sql@gmail.com>
2012-11-18 13:24:28 -0600

    Commented out proc, needs to be re-analyzed and re-coded to match new coreShield coding

--- mcFoodShield/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/dbo.ssoGetUserSystems.proc.sql
+++ mcFoodShield/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/dbo.ssoGetUserSystems.proc.sql
@@ -1,14 +1,14 @@
-﻿CREATE PROCEDURE [dbo].[ssoGetUserSystems]  ( 
-	@email AS NVARCHAR( 100 ) ) AS
-BEGIN
-
-SET NOCOUNT ON;
-
-	SELECT userSystemName, i.legacyID 
-	FROM mcCoreShield.dbo.transitionIdentities AS i
-	INNER JOIN mcCoreShield.dbo.Contacts AS c ON c.id = i.id
-	INNER JOIN mcCoreShield.dbo.transitionSystems AS s
-	ON s.id = i.transitionSystemsID
-	WHERE c.Email = @email;
-
-END
+﻿--CREATE PROCEDURE [dbo].[ssoGetUserSystems]  ( 
+--	@email AS NVARCHAR( 100 ) ) AS
+--BEGIN
+--
+--SET NOCOUNT ON;
+--
+--	SELECT userSystemName, i.legacyID 
+--	FROM mcCoreShield.dbo.transitionIdentities AS i
+--	INNER JOIN mcCoreShield.dbo.Contacts AS c ON c.id = i.id
+--	INNER JOIN mcCoreShield.dbo.transitionSystems AS s
+--	ON s.id = i.transitionSystemsID
+--	WHERE c.Email = @email;
+--
+--END
