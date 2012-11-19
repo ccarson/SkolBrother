bede169d59b8e54a7a0e009c2dc0922d9513a55c
ccarson <chris.carson.sql@gmail.com>
2012-11-18 22:46:44 -0600

    updated vw_transitionContacts

--- mcCoreShield/Schema Objects/Schemas/dbo/Views/dbo.vw_transitionContacts.view.sql
+++ mcCoreShield/Schema Objects/Schemas/dbo/Views/dbo.vw_transitionContacts.view.sql
@@ -1,10 +1,7 @@
-﻿CREATE VIEW [dbo].[vw_transitionContacts] with schemabinding AS 
-	SELECT id
-		, transitionSystemsID
-		, legacyID AS ContactsID
-	FROM dbo.transitionIdentities AS ti
-	WHERE EXISTS (
-		SELECT 1
-		FROM dbo.coreConvertedTables 
-		WHERE id = ti.convertedTableID AND 
-			tableName = 'mc_contact' )
+﻿CREATE VIEW dbo.vw_transitionContacts 
+WITH SCHEMABINDING 
+AS 
+    SELECT  id                  = id
+          , transitionSystemsID = systemID
+          , ContactsID          = portalID
+      FROM  Portal.Contacts ;
