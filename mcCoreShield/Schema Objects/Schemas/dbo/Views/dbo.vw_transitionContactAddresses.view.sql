0757dbffb40d79ffcd1289848c7b9a54798330e5
ccarson <chris.carson.sql@gmail.com>
2012-11-23 14:30:51 -0600

    Modified vw_transitionContactAddresses on mcCoreShield

--- mcCoreShield/Schema Objects/Schemas/dbo/Views/dbo.vw_transitionContactAddresses.view.sql
+++ mcCoreShield/Schema Objects/Schemas/dbo/Views/dbo.vw_transitionContactAddresses.view.sql
@@ -1,10 +1,7 @@
-﻿CREATE VIEW [dbo].[vw_transitionContactAddresses] AS 
-	SELECT id
-		, transitionSystemsID
-		, legacyID AS ContactAddressesID
-	FROM dbo.transitionIdentities AS ti
-	WHERE EXISTS (
-		SELECT * 
-		FROM dbo.coreConvertedTables 
-		WHERE id = ti.convertedTableID AND 
-			tableName = 'mc_contact_addresses' )
+﻿CREATE VIEW dbo.vw_transitionContactAddresses
+WITH SCHEMABINDING
+AS
+    SELECT  id                  = id
+          , transitionSystemsID = systemID
+          , ContactAddressesID  = portalID
+      FROM  Portal.ContactAddresses ;
