fe9c20d91eea5d029a1a02455049957cbe4ff47e
ccarson <chris.carson.sql@gmail.com>
2012-11-23 15:31:42 -0600

    Remove vw_transitionContacts from mcCoreShield and mcFoodShield

--- mcCoreShield/Schema Objects/Schemas/dbo/Views/dbo.vw_transitionContacts.view.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE VIEW dbo.vw_transitionContacts 
-WITH SCHEMABINDING 
-AS 
-    SELECT  id                  = id
-          , transitionSystemsID = systemID
-          , ContactsID          = portalID
-      FROM  Portal.Contacts ;
--- mcFoodShield/Schema Objects/Schemas/dbo/Synonyms/dbo.vw_transitionContacts.synonym.sql
+++
@@ -1,2 +0,0 @@
-﻿CREATE SYNONYM [dbo].[vw_transitionContacts] FOR [mcCoreShield].[dbo].[vw_transitionContacts];
-
