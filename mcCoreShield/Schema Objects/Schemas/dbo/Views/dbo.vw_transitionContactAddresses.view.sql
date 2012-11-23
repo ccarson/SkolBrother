173cadbfb37f1196aa52d356160dfd4e05122336
ccarson <chris.carson.sql@gmail.com>
2012-11-23 15:28:45 -0600

    delete vw_transitionContactAddresses from mcCoreShield and mcFoodShield

--- mcCoreShield/Schema Objects/Schemas/dbo/Views/dbo.vw_transitionContactAddresses.view.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE VIEW dbo.vw_transitionContactAddresses
-WITH SCHEMABINDING
-AS
-    SELECT  id                  = id
-          , transitionSystemsID = systemID
-          , ContactAddressesID  = portalID
-      FROM  Portal.ContactAddresses ;
