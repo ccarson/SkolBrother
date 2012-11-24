9c338a5cbe2f41ce3c66b747f1c69246ed04ca9f
ccarson <chris.carson.sql@gmail.com>
2012-11-23 23:54:56 -0600

    Added Portal.mc_contact_emails and modified dbo.mc_contact_emails on mcFoodShield

---
+++ mcCoreShield/Schema Objects/Schemas/Portal/Views/Portal.mc_contact_emails.view.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW Portal.mc_contact_emails
+AS
+/*
+************************************************************************************************************************************
+
+       View:    Portal.mc_contact_addresses
+     Author:    Chris Carson
+    Purpose:    All portals view of Core.ContactEmails joined on Portal.ContactEmails
+
+    revisor    date            description
+    ---------  -----------     ----------------------------
+    ccarson    ###DATE###      Created
+
+************************************************************************************************************************************
+*/
+
+    SELECT  id              = p.portalID
+          , email           = e.email
+          , user_id         = c.portalID
+          , type_id         = e.typeID
+          , edefault        = e.isDefault   
+          , active          = e.isActive    
+          , epublic         = e.isPublic    
+          , alert           = e.isAlert     
+          , is_emergency    = e.isEmergency 
+          , portalDB        = s.systemDBName
+          , contactID       = e.contactsID
+          , contactEmailsID = e.id
+
+      FROM  Core.ContactEmails   AS e
+INNER JOIN  Portal.ContactEmails AS p ON p.id = e.id
+INNER JOIN  Portal.Contacts      AS c ON c.id = e.contactsID AND c.systemID = p.systemID
+INNER JOIN  dbo.Systems          AS s ON s.id = p.systemID ;
+
--- mcFoodShield/Schema Objects/Schemas/dbo/Views/dbo.mc_contact_emails.view.sql
+++ mcFoodShield/Schema Objects/Schemas/dbo/Views/dbo.mc_contact_emails.view.sql
@@ -1,10 +1,28 @@
-﻿--CREATE VIEW dbo.mc_contact_emails AS SELECT te.ContactEmailsID AS id, e.email, tc.contactsID AS user_id
---				, e.type_id, e.edefault, e.active
---				, e.epublic, e.alert, e.is_emergency
---			FROM dbo.ContactEmails AS e
---			INNER JOIN dbo.vw_transitionContactEmails AS te	ON e.id = te.id
---				AND te.transitionSystemsID = ( SELECT id FROM dbo.transitionSystems 
---												WHERE systemName = db_name() )	 
---			INNER JOIN dbo.vw_transitionContacts AS tc ON e.contactsID = tc.id
---				AND tc.transitionSystemsID = ( SELECT id FROM dbo.transitionSystems 
---												WHERE systemName = db_name() )
+﻿CREATE VIEW dbo.mc_contact_emails
+AS
+/*
+************************************************************************************************************************************
+
+       View:    dbo.mc_contact_emails
+     Author:    Chris Carson
+    Purpose:    Portal view of Core.ContactEmails joined on Portal.ContactEmails
+
+    revisor    date            description
+    ---------  -----------     ----------------------------
+    ccarson    ###DATE###      Created
+
+************************************************************************************************************************************
+*/
+
+    SELECT  id              = v.id
+          , email           = v.email
+          , user_id         = v.user_id
+          , type_id         = v.type_id
+          , edefault        = v.edefault
+          , active          = v.active
+          , epublic         = v.epublic
+          , alert           = v.alert
+          , is_emergency    = v.is_emergency
+
+      FROM  Portal.mc_contact_emails AS v
+     WHERE  v.portalDB = DB_NAME() ;
