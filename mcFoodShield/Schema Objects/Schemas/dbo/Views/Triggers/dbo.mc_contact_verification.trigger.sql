632efb535d9b23038af5b421e1166a218e711ab0
Chris Carson <chris.carson.sql@gmail.com>
2012-11-28 12:13:16 -0600

    modified triggers for portal views

---
+++ mcFoodShield/Schema Objects/Schemas/dbo/Views/Triggers/dbo.mc_contact_verification.trigger.sql
@@ -0,0 +1,60 @@
+﻿CREATE TRIGGER  dbo.tr_mc_contact_verification
+            ON  dbo.mc_contact_verification
+INSTEAD OF INSERT, DELETE, UPDATE
+AS
+/*
+************************************************************************************************************************************
+
+    Trigger:  dbo.tr_mc_contact_verification
+     Author:  Chris Carson
+    Purpose:  Prepares data for coreSHIELD processing
+
+    revisor     date          description
+    ---------   -----------   ----------------------------
+    ccarson     ###DATE###    Created
+
+
+    Logic Summary:
+
+************************************************************************************************************************************
+*/
+BEGIN
+    IF  @@ROWCOUNT = 0 RETURN ;
+
+    SET NOCOUNT ON;
+
+    DECLARE @rc             AS INT
+          , @systemDBName   AS NVARCHAR(50) = DB_NAME()
+          , @operation      AS NVARCHAR(50)
+          , @recordsIN      AS INT
+          , @errorMessage   AS NVARCHAR(MAX) ;
+
+    CREATE TABLE #mc_contact_verification ( id   INT ) ; 
+    
+    EXECUTE @rc = dbo.buildTriggerTable @tableName = 'mc_contact_verification' ; 
+    
+    IF  ( @rc > 0 )
+    BEGIN
+        RAISERROR( 'buildTriggerTable returned non-zero error code', 16, @rc ) ;
+        RETURN ;
+    END
+    
+    INSERT #mc_contact_verification
+    SELECT * FROM inserted i 
+        UNION ALL
+    SELECT * FROM deleted d WHERE NOT EXISTS ( SELECT 1 FROM inserted i WHERE i.id = d.id ) ; 
+    SELECT  @recordsIN = @@ROWCOUNT ;
+
+    IF  EXISTS ( SELECT 1 FROM inserted ) 
+        EXECUTE @rc = Portal.mc_contact_verificationMerge @systemDBName
+                                                        , @recordsIN
+                                                        , @errorMessage OUTPUT ;
+    ELSE
+        EXECUTE @rc = Portal.mc_contact_verificationDelete @systemDBName
+                                                         , @recordsIN
+                                                         , @errorMessage OUTPUT ;
+        
+    IF ( @rc > 0 )
+        RAISERROR ( @errorMessage, 16, @rc ) ;
+        
+END
